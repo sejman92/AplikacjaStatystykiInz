81062ce86d883c64ba9fb918ced1f41df68988d5
sejman92 <sejman92@gmail.com>
2014-10-14 01:11:00 +0200

    Naprawiono. Ten commit przywraca działający projekt z commita: GUI vol2

---
+++ DESKTOP/FootballStatisticCollector/src/fsc/controller/GlobalVariables.java
@@ -0,0 +1,12 @@
+/*
+In this class we collect all necessery variables like paths to resource f.e.
+ */
+package fsc.controller;
+
+/**
+ *
+ * @author Mateusz
+ */
+public class GlobalVariables {
+    public static String guiResource = "/fsc/view/GUI.fxml";
+}
